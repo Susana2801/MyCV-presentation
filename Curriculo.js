ba294eb6427c44d36d60d926fe71e21f4a4f0b60
Susana2801 <124895946+Susana2801@users.noreply.github.com>
2023-02-13 15:48:35 -0300

    Add files via upload

---
+++ Curriculo.js
@@ -0,0 +1,27 @@
+
+
+
+document.getElementById("boton").addEventListener("click", function() {
+	console.log("funciona");
+	document.getElementById("boton").innerHTML = "Escuela de la Familia Agrícola Nº 8221";
+	document.getElementById("boton").style.backgroundColor= "#00ffff";
+});
+
+document.getElementById("boton2").addEventListener("click", function() {
+	
+	document.getElementById("boton2").innerHTML = 
+	"Instituto Superior del Profesorado Nº 4 “Ángel Cárcano";
+	document.getElementById("boton2").style.backgroundColor= "#00ffff";
+});
+
+document.getElementById("boton3").addEventListener("click", function() {
+	
+	document.getElementById("boton3").innerHTML ="Especialización Docente de Nivel Superior en Educación y TIC (Resolución MEN Nº 856/12)."
+	document.getElementById("boton3").style.backgroundColor= "#00ffff";		
+});
+
+document.getElementById("boton4").addEventListener("click", function() {
+
+	document.getElementById("boton4").innerHTML = "2004 a la actualidad: docente de la cátedra de Inglés en diferentes escuelas de Reconquista y zona." ;
+	document.getElementById("boton4").style.backgroundColor= "#00ffff";
+});
